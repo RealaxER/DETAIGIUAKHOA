b1793d697c6a069a75cf5c08258e14e3c428120c
Bùi Hiển <95581165+RealaxER@users.noreply.github.com>
2022-07-12 18:55:43 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "ctdl.h"
+#include"xuly.h"
+void main()
+{
+    //BT Lý thuyết:  trình bày: ds_lop.ds[ds_lop.sl].ds_sv.phead->pnext->data.ds_diem.phead->pnext->data.ma_mh;
+    // ở đây ta trỏ đến kiểu dl danh sashc lớp mở danh sách của nó lên là kiểu hàm nên ta tiếp tục mở số lượng ở kiểu ds lớp
+    //ở đây ta tro đén lớp mỗi lớp thì có một danh sách sinh viên mở danh sách sinh viên lên rồi trỏ đến vị tri sinh viên đầu tiên
+    // là phead rồi trỏ đến sinh viên tiếp theo rồi mở dữ liệu của thằng sinh viên t2 lên , mỗi thằng sinh viên ddeuf có một ds điểm 
+    // sau khi mở danh sach điểm lên ta trỏ đến điểm môn đầu tiên rồi trỏ đến điểm môn thứ 2 rồi mở điểm tp môn t2 lên mỗi tp điểm có nhiều biến nhưng ở đây ta mở mã môn học có diểm đấy
+   
+    menu();
+}
